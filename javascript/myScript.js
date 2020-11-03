b6fcc6598a6e4b2fa3a2dcf94ef6b63decbed192
Pratik-Kumar-621 <70286186+Pratik-Kumar-621@users.noreply.github.com>
2020-11-03 17:14:13 +0530

    Update myScript.js

--- javascript/myScript.js
+++ javascript/myScript.js
@@ -1,13 +1,24 @@
 $(window).on('scroll', function() {
     if ($(window).scrollTop()) {
-        $('nav').addClass('black-nav');
-        $('.list-item a').addClass('black-item');
-        $('.logo .large').addClass('black-large');
-        $('.logo .small').addClass('black-small');
+        $('nav').addClass('change-nav');
+        $('.img-top').addClass('change-img');
+        $('.logo-top div').addClass('change-pad');
+        $('.large').addClass('change-large');
+        $('.small').addClass('change-small');
+        $('.list-item-top a').addClass('decr-pad');
+        $('.sub-list-top').addClass('change-sub');
+        $('.sub-list-top li a').addClass('change-sub-sub');
+
+
     } else {
-        $('nav').removeClass('black-nav');
-        $('.list-item a').removeClass('black-item');
-        $('.logo .large').removeClass('black-large');
-        $('.logo .small').removeClass('black-small');
+        $('nav').removeClass('change-nav');
+        $('.img-top').removeClass('change-img');
+        $('.logo-top div').removeClass('change-pad');
+        $('.large').removeClass('change-large');
+        $('.small').removeClass('change-small');
+        $('.list-item-top a').removeClass('decr-pad');
+        $('.sub-list-top').removeClass('change-sub');
+        $('.sub-list-top li a').removeClass('change-sub-sub');
+
     }
 })
